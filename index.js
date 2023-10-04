e0f1feccd3e0191fbe4dd5d6f4f7b1e16b74c8dc
N3ttX <n3ttx0@gmail.com>
2023-10-04 14:33:14 +0200

    Created Express handlers for static pages
    Again just ported from the old version

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+
+// specify on which port app should be running
+const port = process.env.JS_PORT;
+
+// custom logging
+function log(req, res, next) {
+    console.log("Got a request type: " + req.method);
+    console.warn("Got: " + req.originalUrl);
+    //console.log(req.headers)
+    next();
+}
+
+app.use(log);
+app.use(cors({ origin: "https://opwngrid.xyz" }));
+
+require("./routes/static_routes.js")(app);
+
+// handle exits
+process.on("SIGINT", function() {
+    connection.end(function(err) {
+        process.exit(err ? 1 : 0);
+    });
+});
+
+// start the app
+app.listen(port, () => {
+    console.log(`grid's frontend listening on port ${port}`);
+});

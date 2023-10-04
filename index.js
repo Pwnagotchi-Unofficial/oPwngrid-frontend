ee61f6b477aca32252ab0ef8484e5497faf17f35
N3ttX <n3ttx0@gmail.com>
2023-10-04 17:15:29 +0200

    ESLint new rules
    Added new ESLint rules, and ran eslint --fix on it. Some errors were fixed subsequently. This will make the code more readable hopefully

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const port = process.env.JS_PORT;
 function log(req, res, next) {
     console.log("Got a request type: " + req.method);
     console.warn("Got: " + req.originalUrl);
-    //console.log(req.headers)
+    // console.log(req.headers)
     next();
 }
 
 app.use(log);
-app.use(cors({ origin: ["https://opwngrid.xyz","https://api.opwngrid.xyz"] }));
+app.use(cors({ origin: [ "https://opwngrid.xyz", "https://api.opwngrid.xyz" ] }));
 
 require("./routes/static_routes.js")(app);
 
 // handle exits
 // TODO: Handle this appropriately on other places aswell, ideally also on all the places we make connections (so we can close them)
 process.on("SIGINT", function() {
-    process.exit(err ? 1 : 0);
+    process.exit(0);
 });
 
 // start the app
